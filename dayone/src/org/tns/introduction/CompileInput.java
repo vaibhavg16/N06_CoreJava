e40b0836b2000171646dd69bfd547925916e5534
DELL <DELL@VAIBHAV-GODSE>
2023-07-20 14:07:41 +0530

    first commit

---
+++ dayone/src/org/tns/introduction/CompileInput.java
@@ -0,0 +1,13 @@
+// program to demonstrate on compile time input
+package org.tns.introduction;
+
+public class CompileInput {
+
+	public static void main(String[] args) {
+		
+		short num = 34; // var initialization
+	    System.out.println("the num is: "+num);
+
+	}
+
+}
---
+++ dayone/src/org/tns/introduction/userinput.java
@@ -0,0 +1,20 @@
+//user input demonstration
+package org.tns.introduction;
+import java.util.Scanner;
+public class userinput 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a string 1");
+		String str1=sc.next();
+		System.out.println("String 1 is:"+str1);
+		
+		System.out.println("Enter a string 2");
+		String str2=sc.nextLine();
+		System.out.println("String 2 is:"+str2);
+
+	}
+
+}
