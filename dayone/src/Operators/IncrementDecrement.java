0a7aeee4195a95c32de3385fe384481bfc6df11d
DELL <DELL@VAIBHAV-GODSE>
2023-07-21 14:15:10 +0530

    3rd commit

--- dayone/src/Operators/IncrementDecrement.java
+++ dayone/src/Operators/IncrementDecrement.java
@@ -12,13 +12,19 @@ public class IncrementDecrement
 		Scanner sc=new Scanner(System.in);
 		x=sc.nextInt();
 		y=sc.nextInt();
-	    int res1 = x++ + ++y;
+	    int res1 = x++ + y++;
+	    int res4 = x+y;
+	    System.out.println("x an y "+res4);
 	    System.out.println("x="+x);
+	    System.out.println("y="+y);
 		System.out.println("RESULT for is: "+res1);
         System.out.println("_________________________________________");
 		int res2 = x-- + --y;
 	    System.out.println("x="+x);
-		System.out.println("RESULT for is: "+res2);
+	    System.out.println("y="+y);
+		System.out.println("RESULT 2 is: "+res2);
+		int res3 = --x + --y;
+		System.out.println("RESULT 3 is: "+res3);
 	}
 
 }
